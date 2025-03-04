05ecff4fafd10f0a4ad75d1207500dea5813a534
Dong <20127545@tafe.wa.edu.au>
2025-03-04 20:18:21 +1100

    4-15 customise token w/ jwt callback

--- auth.ts
+++ auth.ts
@@ -56,11 +56,42 @@ export const config = {
       ],
       callbacks: {
         async session({ session, user, trigger, token }: any) {
-          session.user.id = token.sub
-          if (trigger === 'update') {
-            session.user.name = user.name
+          session.user.id = token.sub;
+          session.user.name = token.name;
+          session.user.role = token.role;
+
+          // Optionally handle session updates (like name change)
+          if (trigger === 'update' && token.name) {
+          session.user.name = token.name;
           }
-          return session
+
+          // Return the updated session object
+           return session;
+          },
+
+        async jwt({ token, user, trigger, session }: any) {
+          // Assign user fields to token
+          if (user) {
+            token.role = user.role;
+    
+            // If user has no name, use email as their default name
+            if (user.name === 'NO_NAME') {
+              token.name = user.email!.split('@')[0];
+    
+              // Update the user in the database with the new name
+              await prisma.user.update({
+                where: { id: user.id },
+                data: { name: token.name },
+              });
+            }
+          }
+    
+          // Handle session updates (e.g., name change)
+          if (session?.user.name && trigger === 'update') {
+            token.name = session.user.name;
+          }
+    
+          return token;
         },
       },
   } satisfies NextAuthConfig;
