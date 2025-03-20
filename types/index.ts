d2def16eeca75f7a894ba8c12f5a363eefa2417b
Dong <20127545@tafe.wa.edu.au>
2025-03-20 12:23:15 +1100

    5-2 cart-schema-and-model

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validator';
+import { cartItemSchema, insertCartSchema, insertProductSchema, } from '@/lib/validator';
 
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     createdAt: Date;
